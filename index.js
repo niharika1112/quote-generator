3defdc1da81992dc0f8f11ccac5a5426ab3cb49d
niharika1112 <109222572+niharika1112@users.noreply.github.com>
2025-07-12 17:05:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,432 @@
 const myQuotes =[
+     {
+        quote:"Only one who devotes himself to a cause with his whole syrength and soul can be a true master",
+        author:"Albert Einstein"
+    },
+     {
+        quote:"WWork peacefully and joyfully, knowing that right thoughts and right efforts will inevitably bring about right results",
+        author:"James Allen"
+    },
+     {
+        quote:"It doesn't matter how much you want. what really matter is how much you want it",
+        author:"Ralph Marston"
+    },
+     {
+        quote:"Handle them carefully, for words have more power than atom bombs",
+        author:"Pearl Strachan Hurd"
+    },
+     {
+        quote:"When you feel great, dare to help someone else feels great too",
+        author:"Steve Maraboli"
+    },
+     {
+        quote:"Success is most often achieved by those who don't know failure is inevitable",
+        author:"Coco Chanel"
+    },
+     {
+        quote:"It takes two to speak truth-- one to speak , and another to hear",
+        author:"Henry David Thoreau"
+    },
+     {
+        quote:"Great minds discuss ideas; average mind discuss events; small minds discuss people",
+        author:"Eleanor Roosevelt"
+    },
+     {
+        quote:"There is a time to take counsel of your fears, and there is a time to never listen to any fear",
+        author:"George S. Patton"
+    },
+     {
+        quote:"Develop success from failures. Discouragement and failure are two of the surest stepping stones to success",
+        author:"Dale Carnegie"
+    },
+     {
+        quote:"Big thinking precedes great achievements",
+        author:"Wilferd A. Peterason"
+    },
+     {
+        quote:"If god lived on earth , people would break his windows",
+        author:"Jewish Proverbs"
+    },
+    {
+        quote:"If you don't design your own life plan, chances are you'll fall into someone else's plan. And guess what they have planned for you? Not much",
+        author:"Jim Rohn"
+    },
+     {
+        quote:"Put your heart , mind, intellect and soul even to your smallest acts. This is the secret of success",
+        author:"Siva nanda"
+    },
+     {
+        quote:"It would be rude not to share what I know with those who want to hear it",
+        author:"David Allen"
+    },
+     {
+        quote:"Kites rise highest against the wind, not with it",
+        author:"Winston Churchill"
+    },
+     {
+        quote:"Failure is the condiment that gives success its flavour",
+        author:"Truman Capote"
+    },
+     {
+        quote:"You don't manage people; you manage things. You lead people",
+        author:"Admiral Grace Hooper"
+    },
+     {
+        quote:"It is the heart makes a man rich. He is rich according to what he is, not according to what he has",
+        author:"Henry Ward Beecher"
+    },
+     {
+        quote:"Success is getting what you want, happiness is wanting what you get",
+        author:"W.P. Kinsella"
+    },
+     {
+        quote:"The difference between a successful person and others is not a lack of strength, not a lack of knowledge, but rather a lack in will",
+        author:"Vince Lombardi"
+    },
+     {
+        quote:"The man who questions opinions is wise. The man who quarrels with facts is a fool",
+        author:"Frank Garbutt"
+    },
+     {
+        quote:"If you don't value your time, neither will others, Stop giving away your time and talents. Value what you know& start charging for it",
+        author:"Kim Garst"
+    },
+     {
+        quote:"HE who knows all the answers has not yet been asked all the questions",
+        author:"Niharika"
+    },
+     {
+        quote:"The question isn't who is going to let me; it's who is going to stop me",
+        author:"Ayn Rand"
+    },
+     {
+        quote:"You can't teach a hunter it's wrong to kill",
+        author:"Hari Dass Baba"
+    },
+     {
+        quote:"The captain is not always expected to make the right decision, He is, however, expected to always make the decision",
+        author:"Niharika"
+    },
+     {
+        quote:"Whenever you find yourself on the side of majority, it is time to pause and reflect",
+        author:"Mark Twain"
+    },
+     {
+        quote:"Look to the past for guidance into future.",
+        author:"Robert Jacob Good kin"
+    },
+     {
+        quote:"All you need in this life is ignorance and confidence; then success is sure",
+        author:"Mark Twain"
+    },
+     {
+        quote:"When there is darkness, dare to be the first to shine a light",
+        author:"Steve Maraboli"
+    },
+     {
+        quote:"People often remain in the dark, not due to lack of light but of failure to open their eyes",
+        author:"Allen Cornelius Johnson"
+    },
+     {
+        quote:"Be content to act, and leave the talking to others",
+        author:"Baltasa"
+    },
+    {
+        quote:"The most important thing that parents can teach their children is how to get along without them",
+        author:"Frank A. Clark"
+    },
+    {
+        quote:"Every day I get up and look through the Forbes list of the richest people in America. If I'm not there, I go to work",
+        author:"Vinnie Rege"
+    },
+    {
+        quote:"Politics is the reflex of the business and industrial world ",
+        author:"Emma Goldman "
+    },
+    {
+        quote:"Many of life's failures are people who did not realise how close they were to success when they gave up",
+        author:"Thomas Edison"
+    },
+    {
+        quote:"Maintain peace with men, war with their vices",
+        author:"Me"
+    },
+    {
+        quote:"The successful person makes a habit of doing what the failing person doesn't like to do",
+        author:"Thomas Edison"
+    },
+    {
+        quote:"Always remember when you are on the top of world, that the earth rotates every 24 hrs",
+        author:"Steve G. Makris"
+    },
+    {
+        quote:"As our faith increases, so does our ability to obey",
+        author:"Niharika"
+    },
+    {
+        quote:"We all manufacturers, making good, making trouble, or making excuses",
+        author:"H. V. Adolt"
+    },
+    {
+        quote:"Without knowledge, life is not more than the shadow of death",
+        author:"Molifre"
+    },
+    {
+        quote:"When life gives you a hundred reasons to cry, show life that you have a thousand reason to smile",
+        author:"Niharika"
+    },
+    {
+        quote:"Success is best measured by how far you've come with the talents you've been given",
+        author:"Niharika"
+    },
+    {
+        quote:"Except the best. Prepare for the worst. Capitalize on what comes",
+        author:"Zig Ziglar"
+    },
+    {
+        quote:"The smallest seed of faith is better than the largest fruit of happiness",
+        author:"Henry David Thoreau"
+    },
+    {
+        quote:"Innovation distinguishes between a leader and a follower",
+        author:"Steve Jobs"
+    },
+    {
+        quote:"If your ship doesn't come in, swim out to meet it",
+        author:"Jonathan Winters"
+    },
+    {
+        quote:"A lifetime of happiness? No man could bear it; it would be hell on earth",
+        author:"George Bernard Shaw"
+    },
+    {
+        quote:"A friendship founded on business is a good deal than a business founded on friendship",
+        author:"John D. Rockfeller"
+    },
+    {
+        quote:"When a man finds no peace within himself, it is useless to seek it elsewhere",
+        author:"Francois De La Rochefoucauld"
+    },
+    {
+        quote:"The successful warrior is the average man, with laser-like focus",
+        author:"Bruce Lee"
+    },
+    {
+        quote:"Nature is not only all that is visible to the eye.... it also includes the inner pictures of the soul",
+        author:"Edvard Munch"
+    },
+    {
+        quote:"Set a thief to catch a thief",
+        author:"French Proverbs"
+    },
+    {
+        quote:"Your love will last forever, if you do not expect her to walk the same path, But you look in the same direction",
+        author:"T. Clay Sanders"
+    },
+    {
+        quote:"Accomplishment is hardest when we work the easiest, and easiest when we work the hardest",
+        author:"Niharika"
+    },
+    {
+        quote:"Make friends before you need them",
+        author:"Me"
+    },
+    {
+        quote:"Only those who will risk going too far can possibly find out how far they can go",
+        author:"T.S. Eliot"
+    },
+    {
+        quote:"Normal life cannot sustain revolutionary attitudes for long",
+        author:"Milovan Djilas"
+    },
+    {
+        quote:"A goal is a dream with a deadline",
+        author:"Napoleon  Hill"
+    },
+    {
+        quote:"Know people for who they are rather than for what they are",
+        author:"Anthony J. D 'Angelo"
+    },
+    {
+        quote:"You live longer once you realize that any time spent being unhappy is wasted",
+        author:"Ruth E. Renkl"
+    },
+     {
+        quote:"You can easily judge the character of a man by how he treats those who can do nothing for him",
+        author:"James D. Miles"
+    },
+    {
+        quote:"You must do the things you think you cannot do",
+        author:"Eleanor Roosevelt"
+    },
+    {
+        quote:"Our humanity is a poor thing except for the divinity that stir within us",
+        author:" Francis Bacon"
+    },
+    {
+        quote:"There are two kinds of failure; those who thought and never did, and those who did and never thought",
+        author:"Laurence J. Peter"
+    },
+    {
+        quote:" If I die, I forgive you. If I live we shall see",
+        author:"Spanish Proverbs"
+    },
+    {
+        quote:"Men do less than they ought, unless they do all they can",
+        author:"Thomas Carlyle"
+    },
+    {
+        quote:"He is not great who is not greatly good",
+        author:"William Shakespeare"
+    },
+    {
+        quote:"As I grow to understand life less and less, I learn to love it more and more",
+        author:"Jules Renard"
+    },
+    {
+        quote:"The journey of thousand miles must begin with a single step",
+        author:"Lao Tzu"
+    },
+    {
+        quote:"Some people change when they see the light, others when they feel the heat",
+        author:"Caraoline Schoeder"
+    },
+    {
+        quote:"Try not to become a man of success but a man of value",
+        author:"Albert Einstein"
+    },
+    {
+        quote:"Affliction is often that thing which prepares an ordinary person for some sort of an extra ordinary destiny",
+        author:"Niharika"
+    },
+    {
+        quote:"We are all include to judge ourselves by our ideals; others by their acts",
+        author:"Harold Nicolson"
+    },
+    {
+        quote:"Short lived pleasure is the parent of pain",
+        author:"Proverbs"
+    },
+    {
+        quote:"People forget how fast you did a job, but they remember how well you did it",
+        author:"Howard W. Newton"
+    },
+    {
+        quote:"Persistence is twin sister of excellence. One is a matter of quality; the other, a matter of time",
+        author:"Marabel Morgan"
+    },
+    {
+        quote:"Without goals, and plans to reach them , you are like a ship that has set sail with no destination",
+        author:"Fitzhugh Dodson"
+    },
+    {
+        quote:"The ladder of success is best climbed by stepping on the rungs of opportunity",
+        author:"Ayn Rand"
+    },
+    {
+        quote:"What lies behind us and what lies before us are tiny matters compared to what lies within us",
+        author:"Ralph Waldo Emerson"
+    },
+    {
+        quote:"Responsibility is the price of greatness",
+        author:"Winston churchill"
+    },
+    {
+        quote:" If I told you about a land of love, friend, would you follow me and come?",
+        author:"Yunus Emre"
+    },
+    {
+        quote:"Our main business is not to see that lies dimly in the distance but to do what lies clearly at hand",
+        author:"Thomas Carlyle"
+    },
+    {
+        quote:"Nothing improves the memory more than trying to forget",
+        author:"Niharika"
+    },
+    {
+        quote:"Dream as if you'll live , live as if you'll die today",
+        author:"James Dean"
+    },
+    {
+        quote:"I am a poor man, but I have this consolation : I am poor by accident, not by design",
+        author:"Josh Bilings"
+    },
+    {
+        quote:"Failure is simply the opportunity to begin again, this time more intelligently",
+        author:"Henry Ford"
+    },
+    {
+        quote:"No one can earn a million dollars honestly",
+        author:"William Jennings Brylan"
+    },
+    {
+        quote:"By preserverance the snail reached the ark",
+        author:"Charles H. Spurgeon"
+    },
+    {
+        quote:"Talent wins games, but teamwork and intelligence wins championships",
+        author:"Michael Jordan"
+    },
+    {
+        quote:"The real fault is to have faults and not amend them",
+        author:"Confucius"
+    },
+    {
+        quote:"The men who try to do something and fail are infinetly better than those who try to do nothing and succeed",
+        author:"Lloyd Jones"
+    },
+    {
+        quote:"Look through your customer's eyes. Are you the solution provider or part of the problem",
+        author:"Marlene Blaszczyk"
+    },
+    {
+        quote:"The first and most important step toward success is the feeling that we can succeed",
+        author:"Nelson Boswell"
+    },
+    {
+        quote:"The will to win is important but the will yo prepare is vital",
+        author:"Joe Paterno"
+    },
+    {
+        quote:"Knowing is not enough ; we must apply. Willing is not enough; we must do",
+        author:"Johann Wolfgang von Goethe"
+    },
+    {
+        quote:"Hope is the golden thread that should be woven into every experirnce of life",
+        author:"Niharika"
+    },
+    {
+        quote:" A free man is jealous of his responsibilities as he is of his liberties",
+        author:"Cyril James"
+    },
+    {
+        quote:"A wise man learns by the mistakes of others, a fool by his own",
+        author:"Latin Proverb"
+    },
+    {
+        quote:"Listening to both sides does not necessarily bring about a correct judgement",
+        author:"Donald Rumsfeld"
+    },
+    {
+        quote:"Some men see things as they are and ask why. Others dream things that never were ans ask why not",
+        author:"George Bernard Shaw"
+    },
+    {
+        quote:"When you don't have an education, you've got to use your brains",
+        author:"Niharika"
+    },
+    {
+        quote:"Our duty is to be useful, not according to our desires, but according to our powers",
+        author:"Henry Frederic Amiel"
+    },
+    {
+        quote:"Accountability breeds response-ability",
+        author:"Stephen R. Covey"
+    },
+    {
+        quote:"Leaders must me close to relate to others, but far enough to motivate them",
+        author:"John Maxwell"
+    },
     {
         quote:"When there seems no hope, dare to find some",
         author:"Steve Maraboli"
